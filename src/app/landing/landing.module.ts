e857bff283e65651ec2fb59b5520bb2503c3a236
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2024-08-29 12:24:42 -0600

    feat: base components

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -3,10 +3,22 @@ import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing.component';
 import { HeaderModule } from '../shared/components/header/header.module';
 import { FooterModule } from '../shared/components/footer/footer.module';
+import { ReviewCardModule } from '../shared/components/review-card/review-card.module';
+import { PackageCardModule } from '../shared/components/package-card/package-card.module';
+import { PlaceCardModule } from '../shared/components/place-card/place-card.module';
+import { PackageImageCardModule } from '../shared/components/package-image-card/package-image-card.module';
 
 @NgModule({
   declarations: [LandingComponent],
-  imports: [CommonModule, HeaderModule, FooterModule],
+  imports: [
+    CommonModule,
+    HeaderModule,
+    FooterModule,
+    ReviewCardModule,
+    PackageCardModule,
+    PlaceCardModule,
+    PackageImageCardModule,
+  ],
   exports: [LandingComponent],
 })
 export class LandingModule {}
