d2c4bc2e3d88316b294e8652f2e0d1ce17c9b6ce
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2024-08-30 22:56:04 -0600

    feat: before remove packages

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -11,9 +11,6 @@ export class HeaderComponent {
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const navbar = document.getElementById('navbar');
-    const lugaresID = document.getElementById('lugaresID');
-    const paquetesID = document.getElementById('paquetesID');
-    const aventurasID = document.getElementById('aventurasID');
 
     const targetElement = document.getElementById('target-element');
     if (navbar && targetElement) {
@@ -23,15 +20,9 @@ export class HeaderComponent {
       if (targetPosition <= navbarHeight) {
         this.isColorDark = false;
         navbar.classList.add('bg-dark');
-        lugaresID ? lugaresID.classList.add('text-white') : null;
-        paquetesID ? paquetesID.classList.add('text-white') : null;
-        aventurasID ? aventurasID.classList.add('text-white') : null;
       } else {
         this.isColorDark = true;
         navbar.classList.remove('bg-dark');
-        lugaresID ? lugaresID.classList.remove('text-white') : null;
-        paquetesID ? paquetesID.classList.remove('text-white') : null;
-        aventurasID ? aventurasID.classList.remove('text-white') : null;
       }
     }
   }
